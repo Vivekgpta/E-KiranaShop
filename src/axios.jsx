c3b976792e095cd3dc6abd436289e9cf92a46af7
vivek gupta <131643827+Vivekgpta@users.noreply.github.com>
2025-09-26 13:04:47 +0530

    Update axios.jsx

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/product`,
+  baseURL: `${import.meta.env.VITE_API_URL}`,
 });
 delete API.defaults.headers.common["Authorization"];
 export default API;
+
 
